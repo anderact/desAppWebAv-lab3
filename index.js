3d3e99897b9a73cb85804c2420ae4991a7d93b16
Axel Chacon <axel.chacon@tecsup.edu.pe>
2023-04-05 10:17:30 -0500

    Ejercicio 3

--- index.js
+++ index.js
@@ -2,32 +2,13 @@ const express = require('express')
 const port = 3500
 const app = express()
 
-app.get('/', (req, res)=> {
-    res.send('Hello World')
-})
+//app.use(express.text())
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
 
-app.get('/myfile', (req, res)=> {
-    res.sendFile('express-logo.jpg', {
-        root: __dirname
-    })
-})
-
-app.get('/user', (req, res) => {
-    res.json({
-        nombre: "Axel",
-        apellido: "Chacon",
-        edad: 19,
-        points: [1, 2 ,3],
-        adress: {
-            ciudad: "Lima - Peru",
-            distrito: "San Juan de Lurigancho",
-            calle: "Las Flores"
-        }
-    })
-})
-
-app.get('/isAlive', (req, res) => {
-    res.sendStatus(200)
+app.post('/user', (req, res) => {
+    console.log(req.body)
+    res.send("Nuevo usuario creado")
 })
 
 app.listen(port)
