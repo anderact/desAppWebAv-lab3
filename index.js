12490c316e4a39614f35b02c09ec07688ccc3de0
Axel Chacon <axel.chacon@tecsup.edu.pe>
2023-04-05 10:35:32 -0500

    Ejercicio 4

--- index.js
+++ index.js
@@ -2,13 +2,28 @@ const express = require('express')
 const port = 3500
 const app = express()
 
-//app.use(express.text())
-app.use(express.json())
-app.use(express.urlencoded({extended: false}))
+app.get('/hello/:usernmae', (req, res) => {
+    console.log(typeof req.params.usernmae)
+    res.send(`Hello ${req.params.usernmae.toUpperCase()}`)
+})
 
-app.post('/user', (req, res) => {
-    console.log(req.body)
-    res.send("Nuevo usuario creado")
+app.get('/add/:x/:y', (req, res) => {
+    const {x, y} = req.params
+    res.send(`Result: ${ parseInt(x) + parseInt(y)}`)
+})
+
+app.get('/users/:username/photo', (req, res) => {
+    if(req.params.username.toUpperCase() === "AXEL"){
+        return res.sendFile('./user.jpg', {
+            root: __dirname
+        })
+    }
+
+    res.send('El usuario no tiene acceso !')
+})
+
+app.get('/name/:name/age/:age', (req, res) => {
+    res.send(`El usuario ${req.params.name} tiene ${req.params.age} años`)
 })
 
 app.listen(port)
